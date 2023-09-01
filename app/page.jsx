0a8f5c19230e720385890fd4a00e4e4c530cbd83
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-02 00:36:52 +0630

    app

--- app/page.jsx
+++ app/page.jsx
@@ -1,51 +1,43 @@
 'use client'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, push, set, onValue, remove } from 'firebase/database'
 import styles from './page.module.css'
+import useFirebase from '@/utlis/firebaseApp.jsx'
+import useDatabase from '@/utlis/useDatabase.js'
+import { removeFromDb, addNewToDo, stopListening, listenForValueChange } from '@/utlis/DatabaseFunctions'
 
 export default function Home() {
 
   const [agenda, setAgenda] = useState([])
   const [database, setDatabase] = useState()
+  const [appIntialized, setAppIntialized] = useState(false)
+  const [databaseIntialized, setDatabaseIntialized] = useState(false)
   const [input, setInput] = useState('')
+  const { app: firebaseApp, isAppInitialized } = useFirebase()
+  const { database: firebaseDatabase, isDatabaseInitialized } = useDatabase(firebaseApp, appIntialized);
 
-  const startApp = async () => {
-    const response = await fetch('/api/firebase')
-    const data = await response.json()
-    const firebaseConfig = {
-      databaseURL: data
-    }
-    const app = initializeApp(firebaseConfig);
-    const ref_database = getDatabase(app)
-    setDatabase(ref_database)
-    let userAgenda = ref(ref_database, "To-Dos")
-    onValue(userAgenda, (snapshot) => {
-      // reset()
-      const data = Object.entries(snapshot.val());
-      setAgenda(data)
-    })
-  }
-  const removeFromDb = (id) => {
-    console.log(id)
-    let itemLocationInDb = ref(database, `To-Dos/${id}`)
-    remove(itemLocationInDb)
-  }
-  const AddNewToDo = () => {
-    let itemLocationInDb = ref(database, `To-Dos`)
-    push(itemLocationInDb, input)
-    setInput('')
-  }
   const enterKey = (e) => {
     if (e.key === 'Enter') {
-      AddNewToDo()
+      addNewToDo(firebaseDatabase, input)
     }
   }
-  useEffect(() => {
-    startApp()
-  }, [])
 
+  useEffect(() => {
+    console.log(firebaseApp)
+    setAppIntialized(isAppInitialized)
+  }, [isAppInitialized])
+  useEffect(() => {
+    console.log(firebaseDatabase)
+
+    setDatabaseIntialized(isDatabaseInitialized)
+  }, [isDatabaseInitialized])
+  useEffect(() => {
+    const onDataChange = (updatedData) => {
+      setAgenda(updatedData)
+    }
+    console.log(firebaseDatabase)
+    const listener = listenForValueChange(firebaseDatabase, 'To-Dos', onDataChange, databaseIntialized)
+  }, [databaseIntialized])
   return (
     <main id="web_page">
       <div className="title_box">
@@ -55,8 +47,8 @@ export default function Home() {
         <div className="input_box">
           <input id="new_item_input" name="Add to agenda" type="text" className="input_field"
             placeholder="Add a new item" value={input} onChange={(e) => setInput(e.target.value)} onKeyDown={(e) => enterKey(e)} />
-          <input onClick={AddNewToDo} className="add_btn" type="button" value="Add to list" />
-          <span onClick={AddNewToDo} className="add_symbol">+</span>
+          <input onClick={() => addNewToDo(firebaseDatabase, input)} className="add_btn" type="button" value="Add to list" />
+          <span onClick={() => addNewToDo(firebaseDatabase, input)} className="add_symbol">+</span>
         </div>
       </div>
       <div className="sub_title_box">
@@ -64,8 +56,8 @@ export default function Home() {
           src="./assets/favicons/android-chrome-512x512.png" alt="To do list icon" /></h2>
       </div>
       <div className="agenda_box">
-        {agenda.map((data) => (
-          <div className="agenda_card" onClick={() => removeFromDb(data[0])}>
+        {agenda.map((data, index) => (
+          <div key={index} className="agenda_card" onClick={() => removeFromDb(firebaseDatabase, data[0])}>
             <h4 className="agenda_txt">{data[1]}</h4>
           </div>
         ))}
