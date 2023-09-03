a3d8f485cb63b92c51e6f1f7aab6c71b34520b05
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-03 19:31:41 +0630

    Fixed bugs

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({ index, handleClick, text }) => {
     return (
-        <div key={index} className="agenda_card" onClick={handleClick}>
+        <div className="agenda_card" onClick={handleClick}>
             <h4 className="agenda_txt">{text}</h4>
         </div>
     )
