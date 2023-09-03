3d98a0b0be5731b2ba93fba737dcc8c23bee30ac
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-03 19:32:37 +0630

    Anonymous log in

---
+++ utlis/useAuth.js
@@ -0,0 +1,49 @@
+// useAuth.js
+
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged, signInAnonymously } from 'firebase/auth';
+
+const useAuth = (appIntialized) => {
+    const [user, setUser] = useState(null);
+    const [loggedIn, setLoggedIn] = useState(false)
+    useEffect(() => {
+        console.log(appIntialized)
+
+        if (appIntialized) {
+            console.log(appIntialized)
+            const auth = getAuth()
+            onAuthStateChanged(auth, (authUser) => {
+                if (authUser) {
+                    // User is signed in.
+                    setUser(authUser);
+                } else {
+                    // User is not signed in.
+                    // Sign in anonymously.
+                    try {
+                        signInAnonymously(auth).then((userCredential) => {
+                            const user = userCredential.user
+                            console.log(user)
+                            setUser(user);
+                            setLoggedIn(true)
+                        })
+                            .catch((error) => {
+                                const errorCode = error.code;
+                                const errorMessage = error.message;
+                                console.log(errorCode + errorMessage)
+                                // ...
+                            });;
+
+
+                    } catch (error) {
+                        console.error('Error signing in anonymously:', error);
+                    }
+                }
+            });
+        }
+
+    }, [appIntialized]);
+
+    return { user, loggedIn };
+};
+
+export default useAuth;
