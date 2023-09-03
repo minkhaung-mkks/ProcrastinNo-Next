03a20c61eda870d24040d5d17ed6e9ea47283b0d
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-03 19:41:25 +0630

    Now utilize full perimeter of intialize app

--- utlis/firebaseApp.jsx
+++ utlis/firebaseApp.jsx
@@ -10,16 +10,7 @@ function useFirebase() {
             try {
                 const response = await fetch('/api/firebase');
                 const data = await response.json();
-                const firebaseConfig = {
-                    apiKey: "AIzaSyDNr3yZEDDIE4zcAUqs-Fn9IXeomDAZyis",
-                    authDomain: "to-do-b8cb3.firebaseapp.com",
-                    databaseURL: "https://to-do-b8cb3-default-rtdb.asia-southeast1.firebasedatabase.app",
-                    projectId: "to-do-b8cb3",
-                    storageBucket: "to-do-b8cb3.appspot.com",
-                    messagingSenderId: "782208814642",
-                    appId: "1:782208814642:web:e6b4eeca4af2394b23362b",
-                    measurementId: "G-466ES3W084"
-                };
+                const firebaseConfig = data
                 const appInstance = initializeApp(firebaseConfig);
                 setApp(appInstance);
                 setIsAppInitialized(true); // Set the initialization status to true
