b0e20227d08b62157520e19f93f51d8c7255a327
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-03 19:43:43 +0630

    return the full firebase config now

--- app/api/firebase/route.js
+++ app/api/firebase/route.js
@@ -1,8 +1,25 @@
 
 export const GET = async (req) => {
     try {
+        let apiKey = process.env.API_KEY
+        let authDomain = process.env.AUTH_DOMAIN
         let databaseURL = process.env.DATABASE_URL
-        return new Response(JSON.stringify(databaseURL), { status: 200 })
+        let projectId = process.env.PROJECT_ID
+        let storageBucket = process.env.STORAGE_BUCKET
+        let messagingSenderId = process.env.MESSAGING_SENDER_ID
+        let appId = process.env.APP_ID
+        let measurementId = process.env.MEASUREMENT_ID
+        const firebaseConfig = {
+            apiKey,
+            authDomain,
+            databaseURL: databaseURL,
+            projectId,
+            storageBucket,
+            messagingSenderId,
+            appId,
+            measurementId
+        };
+        return new Response(JSON.stringify(firebaseConfig), { status: 200 })
     }
     catch (err) {
         console.log(err)
